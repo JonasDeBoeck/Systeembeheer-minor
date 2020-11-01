ce80a68821e996d7738bdac61a7c25df7d9e243f
Jonas De Boeck <jonas.deboeck@student.ucll.be>
2020-11-01 16:25:43 +0100

    Toevoeging van functies en service restart

--- scripts/dns_add_record.sh
+++ scripts/dns_add_record.sh
@@ -1,9 +1,43 @@
 #!/bin/bash
+function extract_subdomain_from_fqdn () {
+  SUB="$(cut -d'.' -f1 <<< "$1")"
+}
+
+function db_file_exists() {
+  FILE="/etc/bind/mrt/db.$1"
+  if [[ -f "$FILE"  ]]; then
+    return 1
+  else
+    echo "DB file for this subdomain does not exist"
+    exit 1
+  fi
+}
+
+function create_a_record() {
+  extract_subdomain_from_fqdn "$ZONE"
+  db_file_exists "$SUB"
+  echo "$DOMEIN    IN    A    $IP" >> "/etc/bind/mrt/db.$SUB"
+}
+
+function create_cname_record(){
+  SUB="$(cut -d'.' -f2 <<< "$DOMEIN")"
+  db_file_exists "$SUB"
+  echo "$ALIAS    IN    CNAME    $DOMEIN." >> "/etc/bind/mrt/db.$SUB"
+}
+
+function create_mx_record() {
+  extract_subdomain_from_fqdn "$DOMEIN"
+  db_file_exists "$SUB"
+  echo "        IN    MX    10    $NAME" >> "/etc/bind/mrt/db.$SUB"
+  echo "$NAME    IN    A    $IP" >> "/etc/bind/mrt/db.$SUB"
+}
+
 if [[ $1 != "-t" ]]; then
   RECORD_TYPE="A"
   DOMEIN=$1
   IP=$2
   ZONE=$3
+  create_a_record "$DOMEIN" "$IP" "$SUB"
 else
   while getopts ":t:" opt; do
     case $opt in
@@ -20,17 +54,22 @@ else
         NAME=$3
         IP=$4
         DOMEIN=$5
+        create_mx_record "$NAME" "$IP" "$DOMEIN"
     ;;
     A) echo "$RECORD_TYPE"                                                                                                                                                                                                                          DOMEIN=$3
        IP=$4
        ZONE=$5
+       create_a_record "$DOMEIN" "$IP" "$SUB"
     ;;
     CNAME) echo "$RECORD_TYPE"
            ALIAS=$3
            DOMEIN=$4
+           create_cname_record "$ALIAS" "$DOMEIN"
     ;;
     *) echo "Invalid record type given" >&2
        exit 1
     ;;
   esac
 fi
+
+service bind9 restart
