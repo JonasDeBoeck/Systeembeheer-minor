b7dc689bf78e33606d7a523e1e79f384333f0fed
Jonas De Boeck <jonas.deboeck@student.ucll.be>
2020-11-11 17:00:58 +0100

    Added aanmaken van vhost en configureren

--- scripts/http_add_vhost.sh
+++ scripts/http_add_vhost.sh
@@ -12,4 +12,11 @@ then
   touch "/var/www/$SUBDOMEIN/index.html"
   echo "welcome $SUBDOMEIN" >> "/var/www/$SUBDOMEIN/index.html"
 
-  
+  touch "/etc/apache2/sites-available/$SUBDOMEIN.conf"
+
+  echo "<VirtualHost *:80>" >> "/etc/apache2/sites-available/$SUBDOMEIN.conf"
+  echo "	ServerAdmin webmaster@localhost" >> "/etc/apache2/sites-available/$SUBDOMEIN.conf"
+  echo "	ServerName $SUBDOMEIN.jonas-deboeck.sb.uclllabs.be" >> "/etc/apache2/sites-available/$SUBDOMEIN.conf"
+  echo "	ServerAlias $SUBDOMEIN.jonas-deboeck.sb.uclllabs.be" >> "/etc/apache2/sites-available/$SUBDOMEIN.conf"
+  echo "	DocumentRoot /var/www/$SUBDOMEIN" >> "/etc/apache2/sites-available/$SUBDOMEIN.conf"
+  echo "</VirtualHost>" >> "/etc/apache2/sites-available/$SUBDOMEIN.conf"
