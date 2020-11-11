da478cf03153173d894261a90667aaaa92a0b4a5
Jonas De Boeck <jonas.deboeck@student.ucll.be>
2020-11-11 16:56:04 +0100

    Added www directory structuur aanmaken

--- scripts/http_add_vhost.sh
+++ scripts/http_add_vhost.sh
@@ -3,4 +3,13 @@
 FQDN=$1
 # echo "$FQDN"
 SUBDOMEIN="$(cut -d'.' -f1 <<< "$1")"
-echo "$SUBDOMEIN"
+# echo "$SUBDOMEIN"
+FILENAAM="db.$SUBDOMEIN"
+# echo "$FILENAAM"
+if [-e "/etc/bind/mrt/$FILENAAM" ]
+then
+  mkdir "/var/www/$SUBDOMEIN"
+  touch "/var/www/$SUBDOMEIN/index.html"
+  echo "welcome $SUBDOMEIN" >> "/var/www/$SUBDOMEIN/index.html"
+
+  
